82cf887316af5da3f654cb6954c557dd3cbd23d7
Anthony Styzhin <astyzhin@gmail.com>
2019-11-26 00:08:50 +0300

    Added: RxJava Observer which populates EventsDB and fills RecyclerView with results Updated: MainActivity

--- app/src/main/java/com/akmisoftware/bestpurchases/MainActivity.kt
+++ app/src/main/java/com/akmisoftware/bestpurchases/MainActivity.kt
@@ -1,24 +1,66 @@
 package com.akmisoftware.bestpurchases
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akmisoftware.bestpurchases.db.DataSource
+import com.akmisoftware.bestpurchases.model.Event
+import com.akmisoftware.bestpurchases.ui.recyclerViewItems.EventItem
+import com.google.android.material.snackbar.Snackbar
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Section
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var eventsSection: Section
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private val groupAdapter = GroupAdapter<GroupieViewHolder>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        initRecyclerView()
+        getEvents()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun initRecyclerView() {
+        linearLayoutManager = LinearLayoutManager(this)
+        home_recyclerView.apply {
+            layoutManager = linearLayoutManager
+            adapter = groupAdapter
+        }
+    }
+
+    private fun getEvents() {
+        val eventObservable = Observable
+            .fromIterable(DataSource.createEventsDB())
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse, this::handleError)
+    }
+
+    private fun handleResponse(event: Event) {
+        Log.d("EventSub", "onNext: ${event.name}")
+        groupAdapter.add(EventItem(event))
+    }
+    private fun handleError(error: Throwable) {
+        Log.e("EventSub", error.localizedMessage as String)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
