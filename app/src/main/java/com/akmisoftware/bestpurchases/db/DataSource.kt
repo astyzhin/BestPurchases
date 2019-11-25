5c695d26b06ffceec9d87a17f0287a9c2b95cfd9
Anthony Styzhin <astyzhin@gmail.com>
2019-11-25 20:27:59 +0300

    Added: Basic database DataSource class

---
+++ app/src/main/java/com/akmisoftware/bestpurchases/db/DataSource.kt
@@ -0,0 +1,36 @@
+package com.akmisoftware.bestpurchases.db
+
+import com.akmisoftware.bestpurchases.model.Event
+import com.akmisoftware.bestpurchases.model.Purchase
+import com.akmisoftware.bestpurchases.model.User
+
+class DataSource {
+
+    fun createEventsDB(): ArrayList<Event> {
+    val events = ArrayList<Event>()
+        events.apply {
+            add(Event("Birthday"))
+            add(Event("Party"))
+            add(Event("Celebration"))
+            return events
+        }
+    }
+    fun createUsersBD(): ArrayList<User> {
+        val users = ArrayList<User>()
+        users.apply {
+            add(User("Andy"))
+            add(User("Tom"))
+            add(User("Jim"))
+            return users
+        }
+    }
+    fun createPurchasesDB(): ArrayList<Purchase> {
+        val purchases = ArrayList<Purchase>()
+        purchases.apply {
+            add(Purchase("Toy"))
+            add(Purchase("Box"))
+            add(Purchase("Spinner"))
+            return purchases
+        }
+    }
+}
