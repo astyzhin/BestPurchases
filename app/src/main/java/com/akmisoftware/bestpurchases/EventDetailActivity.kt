f9e3dc5b56b10a760ba34964a8921f9da2a6049b
Anthony Styzhin <astyzhin@gmail.com>
2019-11-26 04:00:35 +0300

    Added: EventDetailActivity menu and it's layout Updated: EventDetailActivity -> populate view items with data from parcelable intent

--- app/src/main/java/com/akmisoftware/bestpurchases/EventDetailActivity.kt
+++ app/src/main/java/com/akmisoftware/bestpurchases/EventDetailActivity.kt
@@ -1,9 +1,13 @@
 package com.akmisoftware.bestpurchases
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.akmisoftware.bestpurchases.db.AppConstants
 import com.akmisoftware.bestpurchases.model.Event
+import kotlinx.android.synthetic.main.activity_event_detail.*
+import java.text.SimpleDateFormat
 
 class EventDetailActivity : AppCompatActivity() {
 
@@ -17,6 +21,33 @@ class EventDetailActivity : AppCompatActivity() {
             title = event?.name
             setDisplayHomeAsUpEnabled(true)
         }
+        setupView()
 
+    }
+
+    private fun setupView() {
+        val dateFormatter = SimpleDateFormat.getDateInstance(SimpleDateFormat.SHORT)
+        if (event != null) {
+
+            event_detail_image.setImageResource(event!!.image)
+            event_date_time_value.text = dateFormatter.format(event?.date) + " " + event?.time
+            event_attendees_value.text = event?.attendees.toString()
+
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_event_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_event_about -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
